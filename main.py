b96c32d1b72a1bda0c50dafa17df462cbf2953b8
Yasser Alhabashi <61691236+xya22er@users.noreply.github.com>
2023-03-09 08:15:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pika 
-from no_ioloop_asynchronous_publisher_example  import ExamplePublisher
+from asynchronous_publisher_example  import ExamplePublisher
 import threading
 import time
 example = ExamplePublisher(
 		'amqp://guest:guest@localhost:5672/%2F?connection_attempts=3&heartbeat=3600'
 	)
+connection_thread = threading.Thread(target=example.connect2(),args=())
+connection_thread.start()
+time.sleep(5)
+channel = example.check_channel()
 
-example.run()
-example.publish_message("msg")
+
+example.message_func("msg1")
+
+example.message_func("msg")
 
