bb63c78c21a4b11bbcffcb318e9fc8ec83dac55a
Yasser Alhabashi <61691236+xya22er@users.noreply.github.com>
2023-03-09 08:40:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 example = ExamplePublisher(
 		'amqp://guest:guest@localhost:5672/%2F?connection_attempts=3&heartbeat=3600'
 	)
-connection_thread = threading.Thread(target=example.connect2(),args=())
+connection_thread = threading.Thread(target=example.connect_to_server(),args=())
 connection_thread.start()
 time.sleep(5)
 channel = example.check_channel()
